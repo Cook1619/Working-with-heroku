fafb531e6526fc71625ab792b025159af1fb7216
Cook1619 <cook6348@gmail.com>
2018-07-23 16:29:05 -0500

    config setup

--- server/src/utils/mail.js
+++ server/src/utils/mail.js
@@ -1,6 +1,6 @@
 import mailgunLoader from 'mailgun-js';
 import { config } from '../config';
-let mailgun = mailgunLoader({ apiKey: config.MAILGUN_API_KEY, domain:'sandbox8fc8d1f3693f480db0b840a52d8110ff.mailgun.org'});
+let mailgun = mailgunLoader({ apiKey: process.env.MAILGUN_API_KEY, domain:'sandbox8fc8d1f3693f480db0b840a52d8110ff.mailgun.org'});
 
 function sendEmail(to, from, subject, content){
     let data = {
