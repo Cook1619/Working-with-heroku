c5d11ad673e9390d1de44c08d3f3f7add9dad9d4
Cook1619 <cook6348@gmail.com>
2018-07-23 16:27:05 -0500

    config setup

--- server/src/utils/stripeCharge.js
+++ server/src/utils/stripeCharge.js
@@ -1,6 +1,6 @@
 import stripeLoader from 'stripe';
 import { config } from '../config/env/production';
-const stripe = stripeLoader(config.STRIPE_SK); // define secret key in config folder
+const stripe = stripeLoader(process.env.STRIPE_SK); // define secret key in config folder
 
 function charge(token, amt) {
     // returning a promise, so when we call .charge elsewhere, we will use await
